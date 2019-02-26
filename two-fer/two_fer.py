4e8a1531f27e9ae955e5f75b71ddb9b800eda028
suchov <artem.suchov@gmail.com>
2019-02-26 09:54:03 +0200

    add some comments to work with python strings in the future

--- two-fer/two_fer.py
+++ two-fer/two_fer.py
@@ -1,2 +1,6 @@
 def two_fer(name = 'you'):
   return 'One for ' + name + ', one for me.'
+
+#  f"One for {name}, one for me."
+#  "One for {}, one for me.".format(name)
+#  "One for %s, one for me." % name
